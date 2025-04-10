c1e76d8c611acfafc1f16b708a8f94cf0c721671
kru <kristoforusrp@gmail.com>
2025-04-10 20:40:21 +0700

    add more token

--- token/token.go
+++ token/token.go
@@ -28,12 +28,33 @@ const (
 
 	// Keywords
 	FUNCTION = "FUNCTION"
-	LET      = "LET"
+	LET		= "LET"
+	TRUE	= "TRUE"
+	FALSE   = "FALSE"
+	IF      = "IF"
+	ELSE    = "ELSE"
+	RETURN  = "RETURN"
+
+	// Operators
+	MINUS	= "-"
+	BANG	= "!"
+	ASTERISK= "*"
+	SLASH	= "/"
+	EQ		= "=="
+	N_EQ	= "!="
+
+	LT = "<"
+	GT = ">"
 )
 
 var keywords = map[string]TokenType{
 	"fn":  FUNCTION,
 	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
